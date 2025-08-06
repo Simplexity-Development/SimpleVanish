f94e0734c3ba93e0cbe3d33a97350ca3b62b92c7
Rhythmic System <mc.adhdmc@gmail.com>
2025-08-06 14:09:58 -0700

    Simplify vanish check and bump major version

--- src/main/java/simplexity/simplevanish/listeners/TrialSpawnListener.java
+++ src/main/java/simplexity/simplevanish/listeners/TrialSpawnListener.java
@@ -1,17 +1,12 @@
 package simplexity.simplevanish.listeners;
 
-import org.bukkit.block.BlockState;
 import org.bukkit.block.TrialSpawner;
-import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.TrialSpawnerSpawnEvent;
-import org.slf4j.Logger;
-import simplexity.simplevanish.SimpleVanish;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Optional;
 
 @SuppressWarnings("UnstableApiUsage")
 public class TrialSpawnListener implements Listener {
@@ -19,17 +14,11 @@ public class TrialSpawnListener implements Listener {
     @EventHandler
     public void onTrialSpawn(TrialSpawnerSpawnEvent spawnEvent) {
         TrialSpawner spawner = spawnEvent.getTrialSpawner();
-        List<Player> trackedPlayers = new ArrayList<>(spawner.getTrackedPlayers().stream().toList());
-        List<Player> modifiedPlayers = new ArrayList<>();
-        for (Player player : trackedPlayers) {
-            if (ListenerUtils.userNotVanished(player)) {
-                modifiedPlayers.add(player);
-            }
-        }
-        if (!modifiedPlayers.isEmpty()) return;
+        Optional<Player> anyModifiedPlayer = spawner.getTrackedPlayers().stream()
+                .filter(ListenerUtils::userNotVanished)
+                .findAny();
+        if (anyModifiedPlayer.isEmpty()) return;
         spawner.setNextSpawnAttempt(0L);
         spawnEvent.setCancelled(true);
     }
-
-
 }
